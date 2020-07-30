7f4f39c8b7717bf6b2953173e3432b969f24a3c3
JeppeSRC <theholyhorse.dev@gmail.com>
2020-07-30 17:00:53 +0200

    Fixed stupid thing I did and added more stuff

--- include/vec3.h
+++ include/vec3.h
@@ -31,6 +31,7 @@ private:
 public:
 	T x, y, z;
 
+
 	_vec3(T init = 0) : x(init), y(init), z(init) {}
 	_vec3(T x, T y, T z) : x(x), y(y), z(z) {}
 
@@ -73,7 +74,7 @@ public:
 
 _vec3<float> _vec3<float>::Normalize() const {
 	_vec3<float> tmp;
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -81,7 +82,7 @@ _vec3<float> _vec3<float>::Normalize() const {
 	if constexpr (alignment) {
 		v = _mm_load_ps(&x);
 	} else {
-		v = _mm_maskload_ps(&x, *(__m128i*) & mask);
+		v = _mm_maskload_ps(&x, mask);
 	}
 
 	__m128 res = _mm_mul_ps(v, v);
@@ -95,14 +96,14 @@ _vec3<float> _vec3<float>::Normalize() const {
 	if constexpr (alignment) {
 		_mm_store_ps(&tmp.x, res);
 	} else {
-		_mm_maskstore_ps(&tmp.x, *(__m128i*)&mask, res);
+		_mm_maskstore_ps(&tmp.x, mask, res);
 	}
 
 	return tmp;
 }
 
 float _vec3<float>::Dot(const _vec3<float>& other) const {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1, v2;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -111,8 +112,8 @@ float _vec3<float>::Dot(const _vec3<float>& other) const {
 		v1 = _mm_load_ps(&x);
 		v2 = _mm_load_ps(&other.x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*)&mask);
-		v2 = _mm_maskload_ps(&other.x, *(__m128i*)&mask);
+		v1 = _mm_maskload_ps(&x, mask);
+		v2 = _mm_maskload_ps(&other.x, mask);
 	}
 	
 	__m128 res = _mm_mul_ps(v1, v2);
@@ -124,7 +125,7 @@ float _vec3<float>::Dot(const _vec3<float>& other) const {
 }
 
 _vec3<float>& _vec3<float>::Add(const _vec3<float>& other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1, v2;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -133,19 +134,23 @@ _vec3<float>& _vec3<float>::Add(const _vec3<float>& other) {
 		v1 = _mm_load_ps(&x);
 		v2 = _mm_load_ps(&other.x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
-		v2 = _mm_maskload_ps(&other.x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
+		v2 = _mm_maskload_ps(&other.x, mask);
 	}
 
 	__m128 res = _mm_add_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
-
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
+	 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Sub(const _vec3<float>& other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1, v2;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -154,19 +159,23 @@ _vec3<float>& _vec3<float>::Sub(const _vec3<float>& other) {
 		v1 = _mm_load_ps(&x);
 		v2 = _mm_load_ps(&other.x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
-		v2 = _mm_maskload_ps(&other.x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
+		v2 = _mm_maskload_ps(&other.x, mask);
 	}
 
 	__m128 res = _mm_sub_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Mul(const _vec3<float>& other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1, v2;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -175,19 +184,23 @@ _vec3<float>& _vec3<float>::Mul(const _vec3<float>& other) {
 		v1 = _mm_load_ps(&x);
 		v2 = _mm_load_ps(&other.x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
-		v2 = _mm_maskload_ps(&other.x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
+		v2 = _mm_maskload_ps(&other.x, mask);
 	}
 
 	__m128 res = _mm_mul_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Div(const _vec3<float>& other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1, v2;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -196,19 +209,23 @@ _vec3<float>& _vec3<float>::Div(const _vec3<float>& other) {
 		v1 = _mm_load_ps(&x);
 		v2 = _mm_load_ps(&other.x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
-		v2 = _mm_maskload_ps(&other.x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
+		v2 = _mm_maskload_ps(&other.x, mask);
 	}
 
 	__m128 res = _mm_div_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Add(float other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -216,19 +233,23 @@ _vec3<float>& _vec3<float>::Add(float other) {
 	if constexpr (alignment) {
 		v1 = _mm_load_ps(&x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
 	}
 
 	__m128 v2 = _mm_set_ps1(other);
 	__m128 res = _mm_add_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Sub(float other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -236,19 +257,23 @@ _vec3<float>& _vec3<float>::Sub(float other) {
 	if constexpr (alignment) {
 		v1 = _mm_load_ps(&x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
 	}
 
 	__m128 v2 = _mm_set_ps1(other);
 	__m128 res = _mm_sub_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Mul(float other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -256,19 +281,23 @@ _vec3<float>& _vec3<float>::Mul(float other) {
 	if constexpr (alignment) {
 		v1 = _mm_load_ps(&x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
 	}
 
 	__m128 v2 = _mm_set_ps1(other);
 	__m128 res = _mm_mul_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<float>& _vec3<float>::Div(float other) {
-	__m128 mask = _mm_set_ps(0, -1.0f, -1.0f, -1.0f);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
 	__m128 v1;
 
 	constexpr bool alignment = alignof(t) % 16 == 0;
@@ -276,13 +305,17 @@ _vec3<float>& _vec3<float>::Div(float other) {
 	if constexpr (alignment) {
 		v1 = _mm_load_ps(&x);
 	} else {
-		v1 = _mm_maskload_ps(&x, *(__m128i*) & mask);
+		v1 = _mm_maskload_ps(&x, mask);
 	}
 
 	__m128 v2 = _mm_set_ps1(other);
 	__m128 res = _mm_div_ps(v1, v2);
 
-	_mm_store_ps(&x, res);
+	if constexpr (alignment) {
+		_mm_store_ps(&x, res);
+	} else {
+		_mm_maskstore_ps(&x, mask, res);
+	}
 
 	return *this;
 }
@@ -297,7 +330,17 @@ _vec3<float>& _vec3<float>::Div(float other) {
 
 _vec3<double> _vec3<double>::Normalize() const {
 	_vec3<double> tmp;
-	__m256d v = _mm256_load_pd(&x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v = _mm256_load_pd(&x);
+	} else {
+		v = _mm256_maskload_pd(&x, mask);
+	}
+
 	__m256d res = _mm256_mul_pd(v, v);
 
 	res = _mm256_hadd_pd(res, res);
@@ -306,14 +349,28 @@ _vec3<double> _vec3<double>::Normalize() const {
 
 	res = _mm256_div_pd(v, res);
 
-	_mm256_store_pd(&tmp.x, res);
+	if constexpr (alignment) {
+		_mm256_store_pd(&tmp.x, res);
+	} else {
+		_mm256_maskstore_pd(&tmp.x, mask, res);
+	}
 
 	return tmp;
 }
 
 double _vec3<double>::Dot(const _vec3<double>& other) const {
-	__m256d v1 = _mm256_load_pd(&x);
-	__m256d v2 = _mm256_load_pd(&other.x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1, v2;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+		v2 = _mm256_load_pd(&other.x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+		v2 = _mm256_maskload_pd(&other.x, mask);
+	}
 
 	__m256d res = _mm256_mul_pd(v1, v2);
 
@@ -324,48 +381,203 @@ double _vec3<double>::Dot(const _vec3<double>& other) const {
 }
 
 _vec3<double>& _vec3<double>::Add(const _vec3<double>& other) {
-	__m256d v1 = _mm256_load_pd(&x);
-	__m256d v2 = _mm256_load_pd(&other.x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1, v2;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+		v2 = _mm256_load_pd(&other.x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+		v2 = _mm256_maskload_pd(&other.x, mask);
+	}
 
 	__m256d res = _mm256_add_pd(v1, v2);
 
-	_mm256_store_pd(&x, res);
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<double>& _vec3<double>::Sub(const _vec3<double>& other) {
-	__m256d v1 = _mm256_load_pd(&x);
-	__m256d v2 = _mm256_load_pd(&other.x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1, v2;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+		v2 = _mm256_load_pd(&other.x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+		v2 = _mm256_maskload_pd(&other.x, mask);
+	}
 
 	__m256d res = _mm256_sub_pd(v1, v2);
 
-	_mm256_store_pd(&x, res);
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<double>& _vec3<double>::Mul(const _vec3<double>& other) {
-	__m256d v1 = _mm256_load_pd(&x);
-	__m256d v2 = _mm256_load_pd(&other.x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1, v2;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+		v2 = _mm256_load_pd(&other.x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+		v2 = _mm256_maskload_pd(&other.x, mask);
+	}
 
 	__m256d res = _mm256_mul_pd(v1, v2);
 
-	_mm256_store_pd(&x, res);
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<double>& _vec3<double>::Div(const _vec3<double>& other) {
-	__m256d v1 = _mm256_load_pd(&x);
-	__m256d v2 = _mm256_load_pd(&other.x);
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1, v2;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+		v2 = _mm256_load_pd(&other.x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+		v2 = _mm256_maskload_pd(&other.x, mask);
+	}
 
 	__m256d res = _mm256_div_pd(v1, v2);
 
-	_mm256_store_pd(&x, res);
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
 
 	return *this;
 }
+
+_vec3<double>& _vec3<double>::Add(double other) {
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+	}
+
+	__m256d v2 = _mm256_set1_pd(other);
+	__m256d res = _mm256_add_pd(v1, v2);
+
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<double>& _vec3<double>::Sub(double other) {
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+	}
+
+	__m256d v2 = _mm256_set1_pd(other);
+	__m256d res = _mm256_sub_pd(v1, v2);
+
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<double>& _vec3<double>::Mul(double other) {
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+	}
+
+	__m256d v2 = _mm256_set1_pd(other);
+	__m256d res = _mm256_mul_pd(v1, v2);
+
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<double>& _vec3<double>::Div(double other) {
+	__m256i mask = _mm256_set_epi64x(0, -1, -1, -1);
+	__m256d v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm256_load_pd(&x);
+	} else {
+		v1 = _mm256_maskload_pd(&x, mask);
+	}
+
+	__m256d v2 = _mm256_set1_pd(other);
+	__m256d res = _mm256_div_pd(v1, v2);
+
+	if constexpr (alignment) {
+		_mm256_store_pd(&x, res);
+	} else {
+		_mm256_maskstore_pd(&x, mask, res);
+	}
+
+	return *this;
+}
+
+
+
 #endif
 #pragma endregion
 
@@ -374,45 +586,197 @@ _vec3<double>& _vec3<double>::Div(const _vec3<double>& other) {
 #ifndef GML_VEC3I_NO_SSE
 
 _vec3<int>& _vec3<int>::Add(const _vec3<int>& other) {
-	__m128i v1 = _mm_load_si128((__m128i*) & x);
-	__m128i v2 = _mm_load_si128((__m128i*) & other.x);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1, v2;
+
+	constexpr int alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((const __m128i*)&x);
+		v2 = _mm_load_si128((const __m128i*)&other.x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+		v2 = _mm_maskload_epi32(&other.x, mask);
+	}
 
 	__m128i res = _mm_add_epi32(v1, v2);
 
-	_mm_store_si128((__m128i*) & x, res);
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<int>& _vec3<int>::Sub(const _vec3<int>& other) {
-	__m128i v1 = _mm_load_si128((__m128i*) & x);
-	__m128i v2 = _mm_load_si128((__m128i*) & other.x);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1, v2;
+
+	constexpr int alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((const __m128i*)&x);
+		v2 = _mm_load_si128((const __m128i*)&other.x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+		v2 = _mm_maskload_epi32(&other.x, mask);
+	}
 
 	__m128i res = _mm_sub_epi32(v1, v2);
 
-	_mm_store_si128((__m128i*) & x, res);
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<int>& _vec3<int>::Mul(const _vec3<int>& other) {
-	__m128i v1 = _mm_load_si128((__m128i*) & x);
-	__m128i v2 = _mm_load_si128((__m128i*) & other.x);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1, v2;
+
+	constexpr int alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((const __m128i*)&x);
+		v2 = _mm_load_si128((const __m128i*)&other.x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+		v2 = _mm_maskload_epi32(&other.x, mask);
+	}
 
 	__m128i res = _mm_mul_epi32(v1, v2);
 
-	_mm_store_si128((__m128i*) & x, res);
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
 
 	return *this;
 }
 
 _vec3<int>& _vec3<int>::Div(const _vec3<int>& other) {
-	__m128i v1 = _mm_load_si128((__m128i*) & x);
-	__m128i v2 = _mm_load_si128((__m128i*) & other.x);
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1, v2;
+
+	constexpr int alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((const __m128i*)&x);
+		v2 = _mm_load_si128((const __m128i*)&other.x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+		v2 = _mm_maskload_epi32(&other.x, mask);
+	}
 
 	__m128i res = _mm_div_epi32(v1, v2);
 
-	_mm_store_si128((__m128i*) & x, res);
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<int>& _vec3<int>::Add(int other) {
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((__m128i*)&x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+	}
+
+	__m128i v2 = _mm_set1_epi32(other);
+	__m128i res = _mm_add_epi32(v1, v2);
+
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);;
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<int>& _vec3<int>::Sub(int other) {
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((__m128i*)&x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+	}
+
+	__m128i v2 = _mm_set1_epi32(other);
+	__m128i res = _mm_sub_epi32(v1, v2);
+
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<int>& _vec3<int>::Mul(int other) {
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((__m128i*)&x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+	}
+
+	__m128i v2 = _mm_set1_epi32(other);
+	__m128i res = _mm_mul_epi32(v1, v2);
+
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
+
+	return *this;
+}
+
+_vec3<int>& _vec3<int>::Div(int other) {
+	__m128i mask = _mm_set_epi32(0, -1, -1, -1);
+	__m128i v1;
+
+	constexpr bool alignment = alignof(t) % 16 == 0;
+
+	if constexpr (alignment) {
+		v1 = _mm_load_si128((__m128i*)&x);
+	} else {
+		v1 = _mm_maskload_epi32(&x, mask);
+	}
+
+	__m128i v2 = _mm_set1_epi32(other);
+	__m128i res = _mm_div_epi32(v1, v2);
+
+	if constexpr (alignment) {
+		_mm_store_si128((__m128i*)&x, res);
+	} else {
+		_mm_maskstore_epi32(&x, mask, res);
+	}
 
 	return *this;
 }
