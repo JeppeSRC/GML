5c7b2aa2be99aba6122ad37d0304e7f5b6deadc1
JeppeSRC <theholyhorse.dev@gmail.com>
2020-08-09 18:46:44 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,14 @@
-#include <gml.h>
+#include <GML/gml.h>
 #include <stdio.h>
 #include <chrono>
 #include <iostream>
 
-typedef vec4d vec;
+using namespace gml;
 
 int main(int argc, char** argv) {
 
-	std::cout << alignof(vec3) << std::endl;
-	std::cout << alignof(vec3d) << std::endl;
-	std::cout << alignof(vec3i) << std::endl << std::endl;
+	vec4 v = gml::Translate(vec3(-1, 1, 10)) * vec4(1, 0, 0, 1);
 
-	std::cout << alignof(vec4) << std::endl;
-	std::cout << alignof(vec4d) << std::endl;
-	std::cout << alignof(vec4i) << std::endl << std::endl;
-
-	constexpr long long iterations = 1000000;
-
-	vec* arr = new vec[iterations];
-
-	for (long long i = 0; i < iterations; i++) {
-		arr[i] = vec(i + 1, i * 2 + 1, i * -100 + 20, 1);
-	}
-
-	auto start = std::chrono::high_resolution_clock::now();
-
-	for (long long i = 0; i < iterations; i++) {
-		arr[i] *= vec(1.0f, 2.0f, 4.0f, 1);
-	}
-
-	auto end = std::chrono::high_resolution_clock::now();
-
-	printf("Time: %fns\n", (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / iterations * 1000);
-
-	for (int i = 0; i < 10; i++) {
-		auto& v = arr[i];
-		printf("%llu %llu %llu\n", v.x, v.y, v.z);
-	}
+	printf("%f %f %f %f\n", v.x, v.y, v.z, v.w);
 
 }
