b04bdc4ac0f8feb5e1df6c1d9b592db3fe82845a
JeppeSRC <theholyhorse.dev@gmail.com>
2020-08-09 17:52:53 +0200

    Added vec3 cross product

--- include/vec3.h
+++ include/vec3.h
@@ -48,6 +48,13 @@ public:
 
 	_vec3<T> Normalize() const { float a = sqrt(x * x + y * y + z * z); return _vec3<T>(x / a, y / a, z / a); }
 	T Dot(const _vec3<T>& other) const { return x * other.x + y * other.y + z * other.z; }
+	_vec3<T> Cross(const _vec3<float>& other) const {
+		T x = y * other.z - z * other.y;
+		T y = z * other.x - x * other.z;
+		T z = x * other.y - y * other.x;
+
+		return _vec3<T>(x, y, z);
+	}
 
 	inline _vec3<T> operator+(const _vec3<T>& other) const { return _vec3<T>(*this).Add(other); }
 	inline _vec3<T> operator-(const _vec3<T>& other) const { return _vec3<T>(*this).Sub(other); }
